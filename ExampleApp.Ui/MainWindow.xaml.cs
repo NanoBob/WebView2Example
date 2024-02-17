1562090b6d8af272326a70fa0ef328f7ca24a533
Bob <bobvanhooff@outlook.com>
2024-02-18 00:02:48 +0100

    Example app, with plain React frontend

---
+++ ExampleApp.Ui/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ExampleApp.Web;
+using System.Diagnostics;
+using System.Windows;
+
+namespace ExampleApp.Ui;
+
+public partial class MainWindow : Window
+{
+    private readonly WebApp webApp = new();
+    private const int portNumber = 20332;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Init();
+    }
+
+    public void Init()
+    {
+        try
+        {
+            var task = Task.Run(() => webApp.Run(portNumber));
+            Webview.Source = new Uri($"http://localhost:{portNumber}?apiPort={portNumber}");
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine(e.Message);
+            if (Debugger.IsAttached)
+                Debugger.Break();
+        }
+    }
+}
---
+++ ExampleApp.Web/WebApp.cs
@@ -0,0 +1,47 @@
+namespace ExampleApp.Web;
+
+public class WebApp
+{
+    private readonly WebApplication app;
+
+    public WebApp()
+    {
+        var builder = WebApplication.CreateBuilder(new WebApplicationOptions()
+        {
+            WebRootPath = "Frontend/build"
+        });
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        app = builder.Build();
+
+        app.UseCors(x =>
+        {
+            x.AllowAnyHeader();
+            x.AllowAnyMethod();
+            x.AllowAnyOrigin();
+        });
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles(new StaticFileOptions()
+        {
+            RequestPath = "",
+        });
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+
+    public void Run(int port) => app.Run($"http://localhost:{port}/");
+}
